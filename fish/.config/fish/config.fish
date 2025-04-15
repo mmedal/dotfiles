d2d1e7786d5159fdc61a1542de44bdeea03b3f3b
Matthew Medal <matt.medal@gmail.com>
2025-04-15 15:39:12 -0700

    Starting dotfiles from scratch

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    # vi mode
+    set fish_cursor_default block
+    set fish_cursor_insert line
+    set fish_cursor_replace_one underscore
+    fish_vi_key_bindings
+
+    # zoxide
+    zoxide init --cmd cd fish | source
+
+    # Starship setup
+    starship init fish | source
+end
+
+# Homebrew setup
+/opt/homebrew/bin/brew shellenv | source
+
+# asdf setup
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
