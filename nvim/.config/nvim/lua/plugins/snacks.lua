62c0a1deb3ca6597f6a3725f2d918424bf6dd46c
Matthew Medal <matt.medal@gmail.com>
2025-04-19 18:14:12 -0700

    Disable explorer keybinds

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,5 +1,11 @@
 return {
   "snacks.nvim",
+  keys = {
+    { "<leader>e", false },
+    { "<leader>E", false },
+    { "<leader>fe", false },
+    { "<leader>fE", false },
+  },
   opts = {
     dashboard = {
       preset = {
