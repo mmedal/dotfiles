2802bc6e801cd33f2245664a2139448a4e9fe79b
Matthew Medal <matt.medal@gmail.com>
2025-04-28 19:26:04 -0700

    Adjust some general keybinds

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Make x always cut to _
+vim.keymap.set({ "n", "v" }, "x", '"_x', { desc = "Delete without yanking" })
+-- Unbind default keywordprg
+vim.keymap.del("n", "<leader>K")
+-- Rebind end of line
+vim.keymap.set({ "n", "v", "o" }, "-", "$", { desc = "End of line" })
