1765ae4f706c1eeff81be7b7debf6bb6aafe75dc
Matthew Medal <matt.medal@gmail.com>
2025-04-28 19:26:13 -0700

    Add yanky

---
+++ nvim/.config/nvim/lua/plugins/yanky.lua
@@ -0,0 +1,11 @@
+return {
+  "gbprod/yanky.nvim",
+  keys = {
+    { "gp", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put Text After (Linewise)" },
+    { "gP", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put Text Before (Linewise)" },
+    { "]p", false },
+    { "[p", false },
+    { "]P", false },
+    { "[P", false },
+  },
+}
